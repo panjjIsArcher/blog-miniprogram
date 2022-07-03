49a12c44ef323902d7f3f659e7cadf1aaa5b24fc
panjiajie <251907882@qq.com>
2022-07-03 11:56:43 -0700

    feat->pxii->panjiajie

--- pages/game/game.js
+++ pages/game/game.js
@@ -12,13 +12,33 @@ Page({
     },
   },
   init(config) {
-     const game = new Game(config)
-     this.data.game = game
-     
+    const game = new Game(config)
+    this.data.game = game
+
+
+  },
+  canvasRender() {
+    const game = this.data.game
+    const pixi = game.pixi
+    const query = wx.createSelectorQuery()
+    const config = this.data.config
+    query.select("#pixi").fields({ node: true, size: true }).exec(res => {
+      const canvas = res[0].node
+      pixi.autoDetectRenderer({
+        width: config.width, 
+        height: config.height,
+        premultipliedAlpha:true,
+        view: canvas, // 注入 canvas
+        transparent: false ,//背景，
+        backgroundColor:0x000000
+      })
+
+    })
   },
   async onLoad(options) {
     // 初始化 pixi
     this.init(this.data.config)
+    this.canvasRender()
   },
-  
+
 });
